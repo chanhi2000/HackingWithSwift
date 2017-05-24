4d4358edbc0babd473089ae83b67f3d2343220be
chanhi2000 <chanhi2000@gmail.com>
2017-05-24 17:25:31 +0900

    project34 created

---
+++ project34/project34/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project34
+//
+//  Created by LeeChan on 5/24/17.
+//  Copyright © 2017 MarkiiimarK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
