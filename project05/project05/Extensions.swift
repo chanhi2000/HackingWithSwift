5f5a11e8928b9510ebf4062ddffff880a1028fdc
chanhi2000 <chanhi2000@gmail.com>
2017-06-16 13:59:20 +0900

    project05 complete

--- project05/project05/Extensions.swift
+++ project05/project05/Extensions.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2017 MarkiiimarK. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+extension UIView {
+    func addConstraintsWithFormat(_ format: String, views: UIView... ) {
+        var viewsDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
