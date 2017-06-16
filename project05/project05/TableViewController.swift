03e86dc6dfa3bf4ec6585bb3df4f8ddde3a3ae63
chanhi2000 <chanhi2000@gmail.com>
2017-06-16 13:59:49 +0900

    project05 successfully made tableview ui

--- project05/project05/TableViewController.swift
+++ project05/project05/TableViewController.swift
@@ -7,19 +7,133 @@
 //
 
 import UIKit
+import GameplayKit
 
-class ViewController: UIViewController {
-
+class TableViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    let wordCellIdentifier = "word"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Word Scramble"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: wordCellIdentifier)
+        
+        assembleWords()
+        startGame()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    fileprivate func assembleWords() {
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                // if startWords are not found, send me back nil instead.
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
     }
+    
+    fileprivate func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] (action: UIAlertAction) in
+            /**
+             *  Unowned reference means the closure can use it but wouldn't make a strong reference
+             *  In this case, we treat / capture self (TableViewControlelr) and ac (UIAlertController)
+             *  as our weak reference to prevent strong reference cycle.
+             */
+            let answer = ac.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        let errorTitle:String
+        let errorMessage:String
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
+                }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
+            }
+        } else {
+            errorTitle = "Word not possible"
+            errorMessage = " You can't spell that word from '\(title!.lowercased())'"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = title!.lowercased()
+        
+        for letter in word.characters {
+            if let pos = tempWord.range(of: String(letter)) {
+                // range(of:) returns an optional position where the item was found
+                tempWord.remove(at: pos.lowerBound)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        // used for spelling check
+        let range = NSMakeRange(0, word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+}
 
-
+extension TableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: wordCellIdentifier, for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 }
 
